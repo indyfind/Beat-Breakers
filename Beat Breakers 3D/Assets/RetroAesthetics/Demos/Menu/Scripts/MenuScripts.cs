ba70ae5ff8cfa0959f6df77bf24dfb70d96dc5b2
Indiana Reed <indyfind@gmail.com>
2017-03-28 03:19:20 -0400

    Added retro package & started playing w/ it

---
+++ Beat Breakers 3D/Assets/RetroAesthetics/Demos/Menu/Scripts/MenuScripts.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RetroAesthetics.Demos {
+
+	public class MenuScripts : MonoBehaviour {
+		public SceneField loadingScene;
+		public SceneField levelScene;
+
+		public bool fadeInMenu = true;
+		public bool fadeOutMenu = true;
+
+		private RetroCameraEffect _cameraEffect;
+		private AsyncOperation _loadingSceneAsync;
+
+		void Start() {
+			if (fadeInMenu) {
+				_cameraEffect = GameObject.FindObjectOfType<RetroCameraEffect>();
+				if (_cameraEffect != null) {
+					_cameraEffect.FadeIn();
+				}
+			}
+		}
+
+		virtual public void StartLevel() {
+			if (levelScene != null) {
+				if (_cameraEffect != null) {
+					if (loadingScene != null) {
+						_loadingSceneAsync = SceneManager.LoadSceneAsync(loadingScene);
+						_loadingSceneAsync.allowSceneActivation = false; 
+					}
+				
+					_cameraEffect.FadeOut(0.5f, LoadNextScene);
+				} else {
+					LoadNextScene();
+				}
+			} else {
+				Debug.LogWarning("Level scene is not set.");
+			}
+		}
+
+		private void LoadNextScene() {
+			if (_loadingSceneAsync != null) {
+				_loadingSceneAsync.allowSceneActivation = true;
+			}
+			SceneManager.LoadSceneAsync(levelScene);
+		}
+	}
+}
---
+++ Beat Breakers 3D/Assets/RetroAesthetics/Scripts/CombineMeshes.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace RetroAesthetics {
+	[ExecuteInEditMode]
+	public class CombineMeshes : MonoBehaviour {
+
+		[InspectorButton("CombineButtonHandler")]
+		public bool combineChildMeshes;
+
+		private void CombineButtonHandler() {
+			CombineChildMeshes();
+		}
+
+		public virtual void CombineChildMeshes() {
+			// Zero transformation is needed because of localToWorldMatrix transform
+			Vector3 position = transform.position;
+			transform.position = Vector3.zero;
+
+			MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+			CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+			Material materialInChild = null;
+			for (int i = 0; i < meshFilters.Length; ++i) {
+				combine[i].mesh = meshFilters[i].sharedMesh;
+				combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+				var renderer = meshFilters[i].gameObject.GetComponent<Renderer>();
+				if (renderer != null) {
+					renderer.enabled = false;
+				}
+				if (materialInChild == null && renderer.sharedMaterial != null) {
+					materialInChild = renderer.sharedMaterial;
+				}
+			}
+
+			// Add mesh filter.
+			var meshFilter = GetComponent<MeshFilter>();
+			if (meshFilter == null) {
+				meshFilter = gameObject.AddComponent<MeshFilter>();
+			}
+			meshFilter.mesh = new Mesh();
+			meshFilter.sharedMesh.CombineMeshes(combine, true, true);
+
+			// Add renderer and set material.
+			var meshRenderer = GetComponent<MeshRenderer>();
+			if (meshRenderer == null) {
+				meshRenderer = gameObject.AddComponent<MeshRenderer>();
+				meshRenderer.sharedMaterial = materialInChild;
+			}
+
+			// Reset position
+			transform.position = position;
+		}
+		
+	}
+}
